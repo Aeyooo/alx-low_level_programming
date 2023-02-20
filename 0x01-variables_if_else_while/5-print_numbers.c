ad83d5b4a2a2d2b6d158008e94914fb7b21568e2
Dosunmu Ayomide Taofiq <ayomideth26@example.com>
2023-02-20 11:19:15 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers 0 to 9
+ * main - Prints numbers 0 to 9.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	
-	for (i=0; i < 10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
